573eef831aaaa0b6cd2a8dd3a85853bf1f501d98
mlgorosito <123603241+mlgorosito@users.noreply.github.com>
2023-01-30 19:45:07 -0300

    Add files via upload

---
+++ cv-proyecto.js
@@ -0,0 +1,55 @@
+
+
+    
+   let a = document.getElementById("demo");
+    
+    document.getElementById("boton").addEventListener('click', function ocultar(){
+        
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    });
+
+    let b = document.getElementById("demo2");
+
+    document.getElementById("boton2").addEventListener('click', function ocultar(){
+        
+        if (b.style.display === "none") {
+            b.style.display = "block";
+        } else {
+            b.style.display = "none";
+        }
+    }); 
+
+    let c = document.getElementById("demo3");
+
+    document.getElementById("boton3").addEventListener('click', function ocultar(){
+        
+        if (c.style.display === "none") {
+            c.style.display = "block";
+        } else {
+            c.style.display = "none";
+        }
+    }); 
+    let d = document.getElementById("demo4");
+
+    document.getElementById("boton4").addEventListener('click', function ocultar(){
+        
+        if (d.style.display === "none") {
+            d.style.display = "block";
+        } else {
+            d.style.display = "none";
+        }
+    }); 
+    let e = document.getElementById("demo5");
+
+    document.getElementById("boton5").addEventListener('click', function ocultar(){
+        
+        if (e.style.display === "none") {
+            e.style.display = "block";
+        } else {
+            e.style.display = "none";
+        }
+    }); 
